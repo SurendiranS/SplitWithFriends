bad34733d78e3d91d1fc917de6511857dbe0b4b3
Surendiran <43315429+SurendiranS@users.noreply.github.com>
2023-07-11 22:33:31 +0530

    UI update and Custom Split feature updated
    1. Added SNO in Transactions table.
    2. Delete button UI changed.
    3. Bug Fixed in upload data.
    4. Custom split entries can be less than Consumers entries, rest entries will be automatically split.

--- script.js
+++ script.js
@@ -119,6 +119,19 @@ function createTransaction(expense, TotalAmount, Spender, Consumers, CustomSplit
   if(customSplit.length === 1 && customSplit[0] === 0){ //check wherether the array is [0]
     customSplit = [];
   }
+  else{
+    const sum = customSplit.reduce((a, b) => a + b, 0);
+    if(consumers.length > customSplit.length && sum < totalAmount){
+      const len = customSplit.length;
+        while (customSplit.length < consumers.length) {
+          customSplit.push((totalAmount-sum)/(consumers.length-len));
+        }
+    }
+    else{
+      alert('1. Custom Split entries must be less than or equal to Consumers.\n2. Sum of Custom Split cannot exceed Total Amount.');
+      return;
+    }
+  }
   // console.log(expense,totalAmount, spender, consumers, customSplit);
   var transaction = new Transaction(expense, totalAmount, spender, consumers, customSplit);
   transactions.push(transaction);
@@ -146,13 +159,14 @@ function displayTransactions(transactions) {
     var split = transaction.customSplit.length === 0 ? transaction.split + ' / Person' : transaction.customSplit;
   const row = document.createElement('tr');
   row.innerHTML = `
+    <td>${sno+1 + '.'}</td>
     <td>${transaction.expense}</td>
     <td>${transaction.totalAmount}</td>
     <td>${transaction.spender}</td>
     <td>${transaction.consumers.join(', ')}</td>
     <td>${split}</td>
     <td>
-      <button type="button" onclick="removeTransaction(${sno})">
+      <button style="border: none;outline:none; padding:0; background:none;" type="button" onclick="removeTransaction(${sno})">
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"  viewBox="0 0 16 16"><path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z"></path></svg>
       </button>
       </button>
@@ -217,7 +231,7 @@ function download(data) {
 
 
 function upload(data){
-  const lines = data.split('\n');
+  const lines = data.trim().split('\n');
     const csv = [];
     lines.forEach(line => {
       const values = [];
